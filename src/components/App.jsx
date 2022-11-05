4f95fae5d135d127b6ef9f600c686f6281de4b35
vidiborets <a.vidiborets@gmail.com>
2022-11-05 10:35:51 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import Painting from './Painting/Painting';
+import paintings from '../data/painting.json';
+import React from 'react';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101'
+    // }}
     >
-      React homework template
+      {paintings.map(painting => (
+        <Painting
+          key={painting.id}
+          url={painting.url}
+          title={painting.title}
+          price={painting.price}
+          authorName={painting.author.tag}
+          profile={painting.author.url}
+          quantity={painting.quantity}
+        />
+      ))}
     </div>
   );
 };
---
+++ src/components/Painting/Painting.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Painting = props => {
+  const { url, title, price, authorName, profile, quantity } = props;
+
+  return (
+    <div>
+      <img src={url} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Автор: <a href={profile}>{authorName}</a>
+      </p>
+      <p>Цена: {price} кредитов</p>
+      <p>Доступность: {quantity < 10 ? 'заканчивается' : 'есть в наличии'}</p>
+      <button type="button">Добавить в корзину</button>
+    </div>
+  );
+};
+
+export default Painting;
+
+PropTypes.propTypes = {
+  url: PropTypes.string,
+  title: PropTypes.string,
+  price: PropTypes.number,
+  profile: PropTypes.string,
+  authorName: PropTypes.string,
+  quantity: PropTypes.number,
+};
